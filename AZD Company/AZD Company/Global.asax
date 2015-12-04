d4d574dc992791ed0f09eedb2802d48ac1a52ce1
Hasitha Prageeth <haztha@gmail.com>
2015-12-04 21:40:31 +0530

    All changes
    Updated Model and some Nueget Packages

---
+++ AZD Company/AZD Company/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AZD_Company.MvcApplication" Language="C#" %>
---
+++ AZD Company/AZD Company/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AZD_Company.Startup))]
+namespace AZD_Company
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
